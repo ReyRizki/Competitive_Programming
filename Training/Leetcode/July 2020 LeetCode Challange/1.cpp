a11c1ef0124ffd42872673c2096f593633553bc4
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-07-02 06:31:55 +0700

    1st July

---
+++ Training/Leetcode/July 2020 LeetCode Challange/1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i = 1;
+        while (n > 0)
+        {
+            n -= i;
+            i++;
+        }
+        return (n < 0) ? i - 2 : i-1;
+    }
+};
