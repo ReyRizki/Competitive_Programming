9efa9256e36eb0a1f09fef323a36d27a969d8347
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-07-31 10:54:15 +0700

    fix: stringStream

--- Training/Hackerrank/C++/String/stringStream.cpp
+++ Training/Hackerrank/C++/String/stringStream.cpp
@@ -1,47 +1,34 @@
-#include <iostream>
-#include <string>
+#include <sstream>
 #include <vector>
-
-#define u unsigned
-#define s short
-#define ll long long
-#define REP(i, a, b) for (auto i = (a); i < (b); i++)
-#define REPR(i, a, b) for (auto i = (a-1); i >= (b); i--)
+#include <iostream>
 
 using namespace std;
 
-vector<int> parseInts(string str) {
-    int temp = 0;
+vector<int> parseInts(string str)
+{
+    stringstream iss(str);
     vector<int> result;
-    int len = str.length();
 
-    REP(i, 0, len)
+    for (int i; iss >> i;)
     {
-        char c = str[i];
-        if (c == '-')
-            temp = -1;
-        else if (c >= '0' and c <= '9')
-            temp = (temp == -1) ? (temp * (c - '0')) : (temp < 0 ? (temp * 10 - (c - '0')) : (temp * 10 + (c - '0')));
-            // temp = (temp * 10 + (c - '0'));
-        // cout << temp << "\n";
-        if ((c == ',') or (i == len - 1))
-        {
-            result.push_back(temp);
-            temp = 0;
-        }
+        result.push_back(i);
+
+        if (iss.peek() == ',')
+            iss.ignore();
     }
 
     return result;
 }
 
-int main() {
+int main()
+{
     string str;
     cin >> str;
-
     vector<int> integers = parseInts(str);
-    for(int i = 0; i < integers.size(); i++) {
+    for (int i = 0; i < integers.size(); i++)
+    {
         cout << integers[i] << "\n";
     }
-    
+
     return 0;
 }
