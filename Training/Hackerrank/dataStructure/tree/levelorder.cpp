e34998bf55929cb10ca5fed640fb85629a506b17
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-08-08 18:54:50 +0700

    add: solved problems

---
+++ Training/Hackerrank/dataStructure/tree/levelorder.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        if (not root)
+            return;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (not q.empty())
+        {
+            Node * temp;
+            temp = q.front();
+
+            if (temp->left)
+                q.push(temp->left);
+            
+            if (temp->right)
+                q.push(temp->right);
+
+            cout << temp->data << " ";
+            q.pop();
+        }
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.levelOrder(root);
+
+    return 0;
+}
