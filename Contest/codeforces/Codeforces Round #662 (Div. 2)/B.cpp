47050a1813963ad55440edb22032813533fbc5a2
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-08-08 09:03:13 +0700

    add: AC

--- Contest/codeforces/Codeforces Round #662 (Div. 2)/B.cpp
+++ Contest/codeforces/Codeforces Round #662 (Div. 2)/B.cpp
@@ -5,50 +5,28 @@
 
 using namespace std;
 
-bool enough(map<int, int> planks, int s, int l, int h)
+bool checker(map<int, int> groupCount)
 {
-    // cout << s << " " << l << " " << h << "\n";
-    if ((s == l) and (s == h))
-        return planks[s] >= 8;
-    else if ((s == l) and (s != h))
-        return (planks[s] >= 6) and (planks[h] >= 2);
-    else if ((s != l) and (s == h))
-        return (planks[s] >= 6) and (planks[l] >= 2);
-    else if ((s != l) and (s != h))
-    {
-        if (l == h)
-            return (planks[s] >= 4) and (planks[l] >= 4);
-        else if (l != h)
-            return (planks[s] >= 4) and (planks[l] >= 2) and (planks[h] >= 2); 
-    }
-}
-
-bool checker(map<int, int> planks)
-{
-    for (auto i = planks.begin(); i != planks.end(); i++)
-        for (auto j = planks.begin(); j != planks.end(); j++)
-            for (auto k = planks.begin(); k != planks.end(); k++)
-                if (enough(planks, i->first, j->first, k->first))
-                    return 1;
-
-    return 0;
+    return (groupCount[8] >= 1) or (groupCount[4] >= 2) or ((groupCount[6] >= 1) and (groupCount[2] >= 2)) or ((groupCount[4] >= 1) and (groupCount[2] >= 3));
 }
 
 void solve()
 {
     int n; cin >> n;
     map<int, int> planks;
+    map<int, int> groupCount;
 
     REP(i, 0, n)
     {
         int a; cin >> a;
         planks[a]++;
+        groupCount[planks[a]]++;
     }
 
-    // for (auto it = planks.begin(); it != planks.end(); ++it)
+    // for (auto it = groupCount.begin(); it != groupCount.end(); ++it)
     //     cout << it->first << " " << it->second << "\n";
 
-    bool prev = 0;
+    // bool prev = 0;
     int q; cin >> q;
     REP(i, 0, q)
     {
@@ -58,21 +36,19 @@ void solve()
         if (c == '+')
         {
             planks[x]++;
-            if (not prev)
-                prev = checker(planks);
+            groupCount[planks[x]]++;
         }
         else if (c == '-')
         {
+            groupCount[planks[x]]--;
             planks[x]--;
-            if (prev)
-                prev = checker(planks);
         }
 
         // cout << c << " " << x << "\n";
         // for (auto it = planks.begin(); it != planks.end(); ++it)
         //     cout << it->first << " " << it->second << "\n";
 
-        cout << (checker(planks) ? "YES" : "NO") << "\n";
+        cout << (checker(groupCount) ? "YES" : "NO") << "\n";
     }
 }
 
