c5ea9f8bdf336479045f586ec3de040b4fd1f5f1
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-10-20 18:13:51 +0700

    AC

---
+++ Training/Hackerrank/math/fundamentals/restaurant.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define REP(i, a, b) for (auto i = (a); i < (b); i++)
+#define REPR(i, a, b) for (auto i = (a-1); i >= (b); i--)
+
+using namespace std;
+
+void solve()
+{
+    int l, b; cin >> l >> b;
+
+    int n = __gcd(l, b);
+
+    cout << (l / n) * (b / n) << "\n";   
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1; cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
