2a0724f02ce88be73bc22481fabcc348f5beefae
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-10-21 09:14:45 +0700

    AC

--- Training/Hackerrank/math/fundamentals/russianPeasant.cpp
+++ Training/Hackerrank/math/fundamentals/russianPeasant.cpp
@@ -1,58 +1,47 @@
-#include <bits/stdc++.h>
-
-#define REP(i, a, b) for (auto i = (a); i < (b); i++)
-#define REPR(i, a, b) for (auto i = (a-1); i >= (b); i--)
-
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-
-pair<long long int, long long int> multiply(pair<long long int, long long int> p1, pair<long long int, long long int> p2, long long int m)
+ll mod;
+ll mmod(ll n)
 {
-    return make_pair(((((p1.first % m) * (p2.first % m)) % m) - (((p1.second % m) * (p2.second % m)) % m)) % m, ((((p1.first % m) * (p2.second % m)) % m) + (((p1.second % m) * (p2.first % m)) % m)) % m);
-}   
-
-pair<long long int, long long int> exponentiate(pair<long long int, long long int> x, long long int n, int m)
+    return (n-(n/mod)*mod);
+}
+pair<ll,ll> mul(pair<ll,ll>p1,pair<ll,ll>p2)
 {
-    while ((n & 1) == 0) 
+    ll ans=mmod(p1.first*p2.first-p1.second*p2.second);
+    ll ansi=mmod(p1.second*p2.first+p1.first*p2.second);
+    return make_pair(ans,ansi);
+}
+pair<ll,ll> binexp(pair<ll,ll> inp,ll kk)
+{
+    pair<ll,ll> ans=make_pair(1,0);
+    
+    while(kk>0)
     {
-        x = multiply(x, x, m);
-        n >>= 1;
+        if(kk&1)
+        {
+            ans=mul(ans,inp);
+        }
+        inp=mul(inp,inp);
+        kk/=2;
     }
-
-    pair<long long int, long long int> P = x;
-
-    n >>= 1;
-    while (n > 0) 
+    
+    return ans;
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int t;
+    cin>>t;
+    while(t--)
     {
-        x = multiply(x, x, m);
-        if ((n & 1) != 0)
-            P = multiply(P, x, m);
+        long long a,b,k;
+        cin>>a>>b>>k>>mod;
+        pair<ll,ll> p;
+        p=make_pair(a,b);
+        pair<ll,ll> answer=binexp(p,k);
+        cout<<(answer.first+mod)%mod<<" "<<(answer.second+mod)%mod<<endl;
         
-        n >>= 1;
     }
-
-    return P;
-}
-
-void solve()
-{
-    long long int a, b; cin >> a >> b;
-    long long int k; cin >> k;
-    long long int m; cin >> m;
-
-    pair<long long int, long long int> p = exponentiate(make_pair(a, b), k, m);
-    cout << p.first << " " << p.second << "\n";
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int t = 1; // cin >> t;
-
-    while (t--)
-        solve();
-
     return 0;
 }
